01494c68a0ba3eb0bf7cf9d4f602fdc891a67e08
Jvilla125 <Jvilla125@gmail.com>
2022-07-23 20:46:31 -0700

    Update the id for each of the rows in the html to have their own specific id. Need to be able to cache each one so it can be manialuted through javascript.

--- js/main.js
+++ js/main.js
@@ -3,38 +3,78 @@ let winner;
 let score;
 
 /*----- app's state (variables) -----*/
-
+const connectFourBoard =  [
+    1, 2, 3, 4, 5, 6, 7, 
+    8, 9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28,
+    29, 30, 31, 32, 33, 34, 35,
+    36, 37, 38, 39, 40, 41, 42
+];
 
 let totalChipCount = 42;
 
 /*----- cached element references -----*/
 const allButtonsEl = {
-    startButton: document.getElementById("Start-Button"),
-    resetButton: document.getElementById("Reset-Button"),
-    playAgainButton: document.getElementById("Play-Again")
+    startButton: document.getElementById("Start-Button").addEventListener('click',init),
+    resetButton: document.getElementById("Reset-Button").addEventListener('click',init),
+    playAgainButton: document.getElementById("Play-Again").addEventListener('click',init)
 };
 
-const playerOneEl = {
-    playerOne: document.querySelector("#PlayerOne"),
-    playerOneImg: document.querySelector("#PlayerOne > img")
+const turnTracker = {
+    playerOne: {
+        playerOne: document.querySelector("#PlayerOne"),
+        playerOneImg: document.querySelector("#PlayerOne > img")
+    },
+    playerTwo: {
+        playerTwo: document.querySelector("#PlayerTwo"),
+        playerTwoImg: document.querySelector("#PlayerTwo > img")
+    }
+};
+
+function test(){
+for (let i = 0; i <= connectFourBoard.length;i++){
+    let some = document.getElementById(`#${connectFourBoard[i]}`);
+    console.log(some)
 }
-const playerTwoEl = {
-    playerTwo: document.querySelector("#PlayerTwo"),
-    playerTwoImg: document.querySelector("#PlayerTwo > img")
 }
 
-const playerOne = document.querySelector("#Playerturntracker > p")
+// const allRows = {
+//     Row1: {
+//         col1: document.getElementById("#1").addEventListener('click', turnBlue),
+//         col2: document.getElementById("#2"),
+//         col3: document.getElementById("#3"),
+//         col4: document.getElementById("#4"),
+//         col5: document.getElementById("#5"),
+//         col6: document.getElementById("#6"),
+//         col7: document.getElementById("#7")
+//     },
+//     Row2:{
+//         col1: document.getElementById("#9"),
+//         col2: document.getElementById("#10"),
+//         col3: document.getElementById("#11"),
+//         col4: document.getElementById("#12"),
+//         col5: document.getElementById("#13"),
+//         col6: document.getElementById("#14"),
+//         col7: document.getElementById("#15")
+//     }, 
+// };
+
+const playerOne = document.querySelector("#Playerturntracker > p");
 
 /*----- event listeners -----*/
 
-document.querySelector("#Reset-Button").addEventListener('click', init);
-document.querySelector("#Start-Button").addEventListener('click', init);
-document.querySelector("#Play-Again").addEventListener('click', init);
+
+
+// Row1.addEventListener('click',function turnBlue(){
+//     Row1.style.background = "blue";
+// });
+
 
 /*----- functions -----*/
 
 
-init()
+
 function init(){
     score = {
         playerOne: 0,
@@ -46,10 +86,38 @@ function init(){
     rendor();
 }
 
+function turnBlue(){
+    for (let row in Row1){
+        if (!row.classList.contains("")){
+            Row1.col1.setAttribute("class","blue");
+        }
+    };
+}
+
+function turnRed(){
+    for (let row in Row1){
+    if (!row.classList.contains("")){
+        row.setAttribute("class","red");
+        }
+    }
+};
+
+
+
+// const exa = document.querySelector("#One")
+// exa.setAttribute("class", "blue")
+
+// Row1.col1.addEventListener('click',turnBlue);
+// Row1.col2.addEventListener('click',turnRed);
+
+
 function rendor(){
 
-    playerOneEl.playerOneImg.src = "imgs/blue-circle.jpg";
-    playerTwoEl.playerTwoImg.src = "imgs/red-circle.png";
+    turnTracker.playerOne.playerOneImg.src = "imgs/blue-circle.jpg";
+    turnTracker.playerTwo.playerTwoImg.src = "imgs/red-circle.png";
 
+
+    turnTracker.playerOne.playerOne.style.border = "5px solid gray";
+    turnTracker.playerTwo.playerTwo.style.border = "5px solid gray";
 }
 
