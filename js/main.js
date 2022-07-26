f5ad9cc5ba7a0a09c70bbe2c818af283d57f5357
Jvilla125 <Jvilla125@gmail.com>
2022-07-26 13:44:48 -0700

    Lines 102-124 inside the rendor function now restricts players from adding a chip anywhere on the board unless there is a chip directly below it. The event listener checks to see if there a peice below with 'columnEl[i].classList.contains('Row1' or 'hasChip')'. If none of the clicked spots has a chip then a class is added to that clicked element.

--- js/main.js
+++ js/main.js
@@ -4,37 +4,35 @@ let score;
 
 /*----- app's state (variables) -----*/
 const connectFourBoard =  [
-    1, 2, 3, 4, 5, 6, 7, 
-    8, 9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28,
-    29, 30, 31, 32, 33, 34, 35,
-    36, 37, 38, 39, 40, 41, 42
+    [1, 2, 3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19, 20, 21],
+    [22, 23, 24, 25, 26, 27, 28],
+    [29, 30, 31, 32, 33, 34, 35],
+    [36, 37, 38, 39, 40, 41, 42]
 ];
-
-let totalChipCount = 42;
-
-// /*----- cached element references -----*/
-// const allButtonsEl = {
-//     startButton: ),
-//     resetButton: document.getElementById("Reset-Button").addEventListener('click',init),
-//     playAgainButton: document.getElementById("Play-Again").addEventListener('click',init)
-// };
-
-const startButton = document.getElementById("Start-Button")
-startButton.addEventListener('click', init)
-
-const resetButton = document.getElementById("Reset-Button");
-resetButton.addEventListener('click', init)
-
-const playAgainButton = document.getElementById("Play-Again");
-playAgainButton.addEventListener('click', init)
-
 const scoreString = {
     playerOne: "Player 1 Score: ",
     playerTwo: "Player 2 Score: ",
     draw: "Draw: "
 }
+
+// /*----- cached element references -----*/
+
+
+const tableEL = document.querySelector("#table");
+const playerOne = document.querySelector("#Playerturntracker > p");
+const square = document.querySelectorAll("#table td");
+const startButton = document.getElementById("Start-Button")
+startButton.addEventListener('click', init)
+const playAgainButton = document.getElementById("Play-Again");
+playAgainButton.addEventListener('click', init)
+const columnEl = document.querySelectorAll("td");
+const rowEl = document.querySelectorAll("tr")
+
+
+
+
 const playerTracker = {
     playerOne: {
         playerEl: document.querySelector("#PlayerOne"),
@@ -48,23 +46,16 @@ const playerTracker = {
         turnString: "Player 2's turn"
     }
 };
-
 const scoreTracker = {
     playerOne: document.querySelector("#P1Score"),
     playerTwo: document.querySelector("#P2Score"),
     draw: document.querySelector("#Draw")
 }
 
-const tableEL = document.querySelector("#table");
-
-const playerOne = document.querySelector("#Playerturntracker > p");
-
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
-
-
 
 function init(){
     score = {
@@ -74,15 +65,14 @@ function init(){
     }
     winner = null;
     startButton.style.display = "none";
-
+    playerTracker.playerTwo.playerEl.style.border = "none"
+   
     rendor();
 
 }
 
-
-
-
-function updatePlayersTurn(){       //this function will update playerones turn to true or false
+//this function will update playerones turn to true or false
+function updatePlayersTurn(){       
     if (playerTracker.playerOne.turn === true){
         playerTracker.playerOne.playerEl.style.border = "none"
         playerTracker.playerTwo.playerEl.style.border = "5px solid gray";
@@ -92,15 +82,11 @@ function updatePlayersTurn(){       //this function will update playerones turn 
         playerTracker.playerTwo.playerEl.style.border = "none";
         playerTracker.playerOne.turn = true;
     }
-
 }
 
-
 function rendor(){
-    // playerTracker.playerOne.playerOneImg.src = "imgs/blue-circle.jpg";
-    // // playerTracker.playerTwo.playerTwoImg.src = "imgs/red-circle.png";
-    
-    resetButton.style.display = "block"
+
+    // resetButton.style.display = "block"
     // startButton.style.display = "block" //This is commented out for now. Will need when there is a winner
 
     playerTracker.playerOne.playerEl.style.border = "5px solid gray";
@@ -112,23 +98,51 @@ function rendor(){
     for (const some in playerTracker){
         playerTracker[some].playerEl.innerText = playerTracker[some].turnString;
     }
-
-    
-    tableEL.addEventListener('click', e => {//this checks to see who's turn it is
-        e.preventDefault() 
-        if (playerTracker.playerOne.turn === true){
-            if (!e.target.classList.contains("")){
-                e.target.setAttribute('class', 'blue');  
-            } 
-        }else{
-            if (!e.target.classList.contains("")){
-                e.target.setAttribute('class', 'red');
-            }
-        }
-        updatePlayersTurn();
-    });
+ 
+    for (let i = 0; i < columnEl.length; i++){
+        columnEl[i].addEventListener('click', e =>{
+        if (columnEl[i].classList.contains("Row1") || (columnEl[i+7].classList.contains("hasChip"))){
+            if(playerTracker.playerOne.turn === true){
+                if(!columnEl[i].classList.contains("blue") && !columnEl[i].classList.contains("red")){
+                    columnEl[i].classList.add('hasChip');
+                    columnEl[i].classList.add('blue');
+                    updatePlayersTurn();
+                } else{
+                    return
+                }
+                }else{
+                    if(!columnEl[i].classList.contains("red") && !columnEl[i].classList.contains("blue")){
+                        columnEl[i].classList.add("hasChip");
+                        columnEl[i].classList.add('red');
+                        updatePlayersTurn();
+                    } else{
+                        return;
+                    }
+                }   
+            }      
+        });
+    }
 
 }
 
-
-
+// tableEL.addEventListener('click', e => {//this checks to see who's turn it is
+//     e.preventDefault() 
+//     if (e.target.id == "table") {
+//         return;
+//     } 
+//     if (playerTracker.playerOne.turn === true){
+//         if(!e.target.classList.contains("blue") && !e.target.classList.contains("red") ){
+//             e.target.classList.add('blue');
+//         }
+//         else {
+//             return;
+//         } 
+//     }else{
+//         if (!e.target.classList.contains("red") && !e.target.classList.contains("blue")){
+//             e.target.classList.add('red');
+//             // console.log(e.target)
+//         }
+//         else {
+//             return;
+//         }
+//     }
